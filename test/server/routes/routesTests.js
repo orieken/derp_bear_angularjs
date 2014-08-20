1f15a732c11d0b9403e2ec3c9085057d3460b185
Oscar Rieken <oscar@ionicsecurity.com>
2014-08-20 12:05:48 -0400

    added mocha to do the server testing, Makefile to: make test, ( •_•)>⌐■-■  (⌐■_■)

---
+++ test/server/routes/routesTests.js
@@ -0,0 +1,20 @@
+var express = require('../');
+var request = require('supertest');
+
+describe('app.route', function() {
+    it('should return a new route', function (done) {
+        var app = express();
+
+        app.route('/foo')
+            .get(function (req, res) {
+                res.send('get');
+            })
+            .post(function (req, res) {
+                res.send('post');
+            });
+
+        request(app)
+            .post('/foo')
+            .expect('post', done);
+    });
+});
